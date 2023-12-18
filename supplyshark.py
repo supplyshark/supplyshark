dacdbb46371e4db2ad5cd7ccb2bb1a9f5afff38d
Cody Zacharias <22013186+haccer@users.noreply.github.com>
2023-12-18 10:53:17 -0500

    Add read file (-L)

--- supplyshark.py
+++ supplyshark.py
@@ -7,9 +7,9 @@ import shark
 def chunk(ls, n):
     return [ls[i::n] for i in range(n)]
 
-def run(repos, user, output):
+def run_thread(repos, user, output):
     for repo in repos:
-        print(f"[+] Downloading {repo}")
+        print(f"[+] Downloading {user}/{repo}")
         path = shark.github.clone_repo(user, repo)
     
         t1 = Thread(target=shark.npm.main, args=(path, repo, output,))
@@ -32,26 +32,37 @@ def run(repos, user, output):
     
         shark.file.del_folder(path)
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-u", type=str, required=True)
-    parser.add_argument("-o", type=str, required=True)
-    args = parser.parse_args()
-    tmp = f"/tmp/.supplyshark/{args.u}"
+def run(user, output):
+    tmp = f"/tmp/.supplyshark/{user}"
     Path(tmp).mkdir(parents=True, exist_ok=True)
-    repos = shark.github.get_repos(args.u)
-
-    cpu = multiprocessing.cpu_count()
-    print(f"[+] Starting SupplyShark with {cpu} processors")
-    c = chunk(repos, cpu)
+    repos = shark.github.get_repos(user)
+    
+    c = chunk(repos, multiprocessing.cpu_count())
 
     procs = []
     for i, s in enumerate(c):
-        proc = multiprocessing.Process(target=run, args=(s, args.u, args.o))
+        proc = multiprocessing.Process(target=run_thread, args=(s, user, output))
         procs.append(proc)
         proc.start()
-    
+
     for p in procs:
         p.join()
 
     shark.file.del_folder(tmp)
+
+def run_file(file, output):
+    with open(file, "r") as f:
+        for ff in f.readlines():
+            run(ff.strip(), output)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", type=str)
+    parser.add_argument("-o", type=str, required=True)
+    parser.add_argument("-L", type=str)
+    args = parser.parse_args()
+    
+    if args.L is not None:
+        run_file(args.L, args.o)
+    else:
+        run(args.u, args.o)
